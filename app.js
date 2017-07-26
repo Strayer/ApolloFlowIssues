d98a0a7681a86ce5aa0c4d9acb67428b22f041a5
Sven Grunewaldt <strayer@olle-orks.org>
2017-07-26 17:05:05 +0200

    Add component with state

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
 // @flow
 import React, { PureComponent } from "react";
 import { Button, View, Text } from "react-native";
-import { createStore } from 'redux';
-import { connect, Provider } from 'react-redux';
-import type { Store } from 'redux';
-import type { Dispatch } from "redux";
+// $FlowFixMe
 import { ApolloClient, createNetworkInterface, ApolloProvider, gql, graphql } from 'react-apollo';
-import type { OperationComponent } from "react-apollo";
-
-type State = number;
-type Action = {type: "INCREMENT"} | {type: "DECREMENT"};
+import type { OperationComponent, ChildProps } from "react-apollo";
 
 type AllTrips = Array<{id: string}>;
 type Response = { allTrips: AllTrips };
-
-const reducer: (State, Action) => State = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-};
-const store: Store<State, Action> = createStore(reducer);
-
-const mapStateToProps = (state: State) => {
-    return {
-        counter: state,
-    };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch<*>) => {
-    return {
-        onIncrement: () => {
-            dispatch({type:"INCREMENT"})
-        },
-    };
-};
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
@@ -46,34 +14,22 @@ const client = new ApolloClient({
   }),
 });
 
-class TheButton extends PureComponent {
-    props: {
-        counter: number,
-        onIncrement: () => void,
+type State = { counter: number };
+
+class TheGraphQLThing extends PureComponent<void, ChildProps<{}, Response>, State> {
+    state = {
+        counter: 0,
     };
     
-    render() {
-        const {
-            counter,
-            onIncrement,
-        } = this.props;
-        
-        return <Button
-            onPress={onIncrement} 
-            title={`Increment from ${counter} to ${counter+1}`} 
-        />
-        ;
-    }
-}
-
-const TheButtonContainer = connect(mapStateToProps, mapDispatchToProps)(TheButton);
-
-class TheGraphQLThing extends PureComponent {
     render() {
         const {
             loading,
             allTrips,
         } = this.props.data;
+        
+        const {
+            counter,
+        } = this.state;
         
         if (loading) {
             return <Text>Loading</Text>;
@@ -82,7 +38,15 @@ class TheGraphQLThing extends PureComponent {
         // $FlowFixMe remove this comment to test prop types
         const shouldCauseAFlowError: number = allTrips[0].test;
         
-        return <Text>Number of trips: {allTrips.length}</Text>;
+        return (
+            <View style={{alignItems: "center", justifyContent: "center"}}>
+                <Text>Number of trips: {allTrips.length}</Text>
+                <Button
+                    onPress={() => this.setState({ counter: counter + 1 })}
+                    title={`Increment ${counter} to ${counter+1}`}
+                />
+            </View>
+        );
     }
 }
 
@@ -93,14 +57,11 @@ const TheGraphQLThingContainer = withQuery(TheGraphQLThing);
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <ApolloProvider client={client}>
-                <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-                    <TheButtonContainer />
-                    <TheGraphQLThingContainer />
-                </View>
-            </ApolloProvider>
-        </Provider>
+        <ApolloProvider client={client}>
+            <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+                <TheGraphQLThingContainer />
+            </View>
+        </ApolloProvider>
     )
 };
 
